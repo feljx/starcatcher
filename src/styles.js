8177b1359b30b60e4f2f4d17dd706ebfcc9aa67a
felix <11788526+flxsu@users.noreply.github.com>
2021-07-16 11:25:15 +0200

    this runs

--- src/styles.js
+++ src/styles.js
@@ -8,105 +8,105 @@ export const CELL_SIZE = RAW_CELL_SIZE.toString() + 'rem'
 export const ZOOMED_FONT_SIZE = (RAW_CELL_SIZE * 1.5).toString() + 'rem'
 
 const NO_USER_SELECT = {
-	'-ms-user-select': 'none',
-	'-moz-user-select': 'none',
-	'-webkit-user-select': 'none',
-	userSelect: 'none',
+    '-ms-user-select': 'none',
+    '-moz-user-select': 'none',
+    '-webkit-user-select': 'none',
+    userSelect: 'none'
 }
 const PROTO_CELL_STYLES = {
-	width: CELL_SIZE,
-	height: CELL_SIZE,
-	fontSize: '1.8rem',
-	fontFamily: 'Arial',
-	fontWeight: 'bold',
-	textAlign: 'center',
-	cursor: 'pointer',
-	...NO_USER_SELECT,
+    width: CELL_SIZE,
+    height: CELL_SIZE,
+    fontSize: '1.8rem',
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    cursor: 'pointer',
+    ...NO_USER_SELECT
 }
 
 export const body_styles = {
-	backgroundColor: STARRYNIGHT,
-	fontFamily: 'Times, serif',
+    backgroundColor: STARRYNIGHT,
+    fontFamily: 'Times, serif',
 
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	alignContent: 'flex-start',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignContent: 'flex-start',
 
-	'> *': {
-		marginTop: '1rem',
-	},
+    '> *': {
+        marginTop: '1rem'
+    }
 }
 
 export const title_styles = {
-	color: STARRYGOLD,
-	fontSize: '4rem',
-	marginTop: '2rem',
-	// lineHeight: '0',
+    color: STARRYGOLD,
+    fontSize: '4rem',
+    marginTop: '2rem'
+    // lineHeight: '0',
 }
 
 export const game_styles = {
-	color: STARRYGOLD,
-	display: 'flex',
-	marginLeft: '-10rem',
+    color: STARRYGOLD,
+    display: 'flex',
+    marginLeft: '-10rem'
 }
 
 export const sidebar_styles = {
-	width: '6rem',
-	fontSize: '3.2rem',
-	// fontFamily: 'Helvetica',
-	// fontWeight: 'bold',
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'flex-start',
-	marginRight: '4rem',
+    width: '6rem',
+    fontSize: '3.2rem',
+    // fontFamily: 'Helvetica',
+    // fontWeight: 'bold',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginRight: '4rem',
 
-	'> *': {
-		padding: '0.6rem',
-	},
+    '> *': {
+        padding: '0.6rem'
+    },
 
-	'> *:not(:first-child)': {
-		marginTop: '3rem',
-	},
+    '> *:not(:first-child)': {
+        marginTop: '3rem'
+    }
 }
 
 export const smile_styles = {
-	padding: '0.6rem',
-	cursor: 'pointer',
-	':hover': {
-		background: STARRYGOLD,
-		color: STARRYNIGHT,
-	},
+    padding: '0.6rem',
+    cursor: 'pointer',
+    ':hover': {
+        background: STARRYGOLD,
+        color: STARRYNIGHT
+    }
 }
 
 export const grid_styles = {
-	color: STARRYGOLD,
+    color: STARRYGOLD,
 
-	display: 'grid',
-	gridGap: '0.3rem',
-	gridTemplateColumns: `repeat(${CELL_NUM}, 1fr)`,
-	gridTemplateColumns: `repeat(${CELL_NUM}, 1fr)`,
+    display: 'grid',
+    gridGap: '0.3rem',
+    gridTemplateColumns: `repeat(${CELL_NUM}, 1fr)`,
+    gridTemplateColumns: `repeat(${CELL_NUM}, 1fr)`
 }
 
 export const cell_styles = {
-	...PROTO_CELL_STYLES,
-	background: STARRYGOLD,
-	color: STARRYNIGHT,
+    ...PROTO_CELL_STYLES,
+    background: STARRYGOLD,
+    color: STARRYNIGHT,
 
-	':hover': {
-		color: STARRYGOLD,
-		background: STARRYFIRE,
-	},
+    ':hover': {
+        color: STARRYGOLD,
+        background: STARRYFIRE
+    }
 }
 
 export const open_cell_styles = {
-	...PROTO_CELL_STYLES,
-	background: STARRYNIGHT,
-	color: STARRYGOLD,
+    ...PROTO_CELL_STYLES,
+    background: STARRYNIGHT,
+    color: STARRYGOLD
 }
 
 export const flagged_cell_styles = {
-	...PROTO_CELL_STYLES,
-	background: STARRYFIRE,
-	color: STARRYGOLD,
+    ...PROTO_CELL_STYLES,
+    background: STARRYFIRE,
+    color: STARRYGOLD
 }
